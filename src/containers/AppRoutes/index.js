fe43f17949176bfc3da08651774bd8cfaee44aff
Manuel Venegas <mengasis@outlook.com>
2017-08-06 22:20:27 -0400

    Add Component Page404

--- src/containers/AppRoutes/index.js
+++ src/containers/AppRoutes/index.js
@@ -2,10 +2,12 @@ import React from 'react'
 import { Route, Switch } from 'react-router-dom'
 
 import Home from '../Home'
+import Page404 from '../../components/Page404'
 
 const AppRoutes = () =>
 	<Switch>			
 		<Route exact path="/" component={Home} />
+		<Route component={Page404}/>
 	</Switch>
 
 
