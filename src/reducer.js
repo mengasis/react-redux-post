cf832332c23459f7f4d0df34f362692d741ce723
Manuel Venegas <mengasis@outlook.com>
2017-08-27 21:06:24 -0300

    Divide reducers with combineReducer

--- src/reducer.js
+++ src/reducer.js
@@ -1,4 +1,5 @@
 import types from './actionTypes'
+import {combineReducers} from 'redux'
 
 const initialState = {
 	posts: {
@@ -9,21 +10,54 @@ const initialState = {
 	users: {},
 }
 
-function reducer(state = initialState, action = {}){
+function postPageReducer(state = initialState.posts.page, action = {}){
 	switch (action.type) {
 	case types.SET_POST:
-            
-		return Object.assign({}, state, {
-			posts: Object.assign({}, state.posts, {
-				entities: state.posts.entities.concat(action.payload),
-				page: state.posts.page + 1
-			})
-		})
-    
+		return state + 1
 	default:
 		return state
 	}
 }
+
+function postEntitiesReducer(state = initialState.posts.entities, action = {}){
+	switch (action.type) {
+	case types.SET_POST:
+		return state.concat(action.payload)
+	default:
+		return state
+	}
+}
+
+function commentsReducer(state = initialState.comments, action = {}){
+	switch (action.type) {
+	case types.SET_COMMENTS:
+		return state.concat(action.payload)
+	default:
+		return state
+	}
+}
+
+function usersReducer(state = initialState.users, action = {}){
+	switch (action.type) {
+	case types.SET_USER:	
+		return Object.assign({}, state, {
+			[action.payload.id]: action.payload
+		})
+	default:
+		return state
+	}
+}
+
+const postsReducer = combineReducers({
+	page: postPageReducer,
+	entities: postEntitiesReducer
+})
+
+const reducer = combineReducers({
+	posts: postsReducer,
+	comments: commentsReducer,
+	users: usersReducer
+})
 
 export default reducer
 
