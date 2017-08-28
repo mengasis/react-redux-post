d8341a8a938e00cc365b5a073a60409da1a86755
Manuel Venegas <mengasis@outlook.com>
2017-08-28 01:00:03 -0300

    Added ImmutableJS with the actions and reducers

--- src/reducer.js
+++ src/reducer.js
@@ -1,16 +1,17 @@
 import types from './actionTypes'
-import {combineReducers} from 'redux'
+import { combineReducers } from 'redux-immutable'
+import { fromJS, Map as map } from 'immutable'
 
-const initialState = {
+const initialState = fromJS({
 	posts: {
 		page: 1,
-		entities: [],
+		entities: {},
 	},
-	comments: [],
+	comments: {},
 	users: {},
-}
+})
 
-function postPageReducer(state = initialState.posts.page, action = {}){
+function postPageReducer(state = initialState.get('posts').get('page'), action = {}){
 	switch (action.type) {
 	case types.SET_POST:
 		return state + 1
@@ -19,30 +20,35 @@ function postPageReducer(state = initialState.posts.page, action = {}){
 	}
 }
 
-function postEntitiesReducer(state = initialState.posts.entities, action = {}){
+function postEntitiesReducer(state = initialState.get('posts').get('entities'), action = {}){
 	switch (action.type) {
 	case types.SET_POST:
-		return state.concat(action.payload)
+		return action.payload
+			.reduce(
+				(posts, post) => posts.set(post.id, map(post)),
+				state
+			)
 	default:
 		return state
 	}
 }
 
-function commentsReducer(state = initialState.comments, action = {}){
+function commentsReducer(state = initialState.get('comments'), action = {}){
 	switch (action.type) {
 	case types.SET_COMMENTS:
-		return state.concat(action.payload)
+		return action.payload
+			.reduce((comments, comment) => comments.set(comment.id, map(comment)),
+				state
+			)
 	default:
 		return state
 	}
 }
 
-function usersReducer(state = initialState.users, action = {}){
+function usersReducer(state = initialState.get('users'), action = {}){
 	switch (action.type) {
-	case types.SET_USER:	
-		return Object.assign({}, state, {
-			[action.payload.id]: action.payload
-		})
+	case types.SET_USER:
+		return state.set(action.payload.id, map(action.payload))	
 	default:
 		return state
 	}
