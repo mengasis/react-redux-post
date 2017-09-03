52232c6fbb4e58cc430e9ddaa8258acf083163b4
Manuel Venegas <mengasis@outlook.com>
2017-09-03 20:44:55 -0300

    Added Redux to Profile Component

--- src/pages/containers/Profile/index.js
+++ src/pages/containers/Profile/index.js
@@ -1,60 +1,54 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import { Link } from 'react-router-dom'
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
 
-import api from '../../../../lib/originalApi'
+import actions from '../../../actions'
 
 class Profile extends Component {
 
 	constructor(props){
 		super(props)
 		this.state = {
-			loading: true,
-			user: {}
+			loading: true
 		}
 	}
     
 	async componentDidMount() {
 
 		document.title = 'Profile Detail'				
-
-		const [
-			user
-		] = await Promise.all([
-			api.users.getUser(this.props.match.params.id)
-		])
-		this.setState({
-			user,
-			loading: false,
-		})
+		await this.props.actions.loadUser(this.props.match.params.id)
+		
+		this.setState({loading: false})
 	}
 
 
 	render() {
 
-		const { user } = this.state
-
+		const { user } = this.props
+		console.log(user)
 		return (
 			<div>
 				<div>
-					<h1>Profile: {user.name}</h1>
+					<h1>Profile: {user.get('name')}</h1>
 					<h2></h2>
 					<fieldset>
 						<legend><strong>Informacion Basica</strong></legend>
-						<p><strong>Usuario: </strong>{user.username}</p>
-						<p><strong>Email: </strong>{user.email}</p>
-						<p><strong>Telefono: </strong>{user.phone}</p>
-						<p><strong>Sitio: </strong>{user.website}</p>
+						<p><strong>Usuario: </strong>{user.get('username')}</p>
+						<p><strong>Email: </strong>{user.get('email')}</p>
+						<p><strong>Telefono: </strong>{user.get('phone')}</p>
+						<p><strong>Sitio: </strong>{user.get('website')}</p>
 					</fieldset>
 
-					{user.address && ( 
+					{user.get('address') && ( 
 						<fieldset>
 							<legend><strong>Dirección</strong></legend>
-							<p><strong>Calle: </strong>{user.address.street}</p>
-							<p><strong>Suite: </strong>{user.address.suite}</p>
-							<p><strong>Ciudad: </strong>{user.address.city}</p>
-							<p><strong>Codigo Postal: </strong>{user.address.zipcode}</p>
-							<p><strong>Geolocalizacion: </strong>{`${user.address.geo.lat} , ${user.address.geo.lng}`}</p>
+							<p><strong>Calle: </strong>{user.get('address').street}</p>
+							<p><strong>Suite: </strong>{user.get('address').suite}</p>
+							<p><strong>Ciudad: </strong>{user.get('address').city}</p>
+							<p><strong>Codigo Postal: </strong>{user.get('address').zipcode}</p>
+							<p><strong>Geolocalizacion: </strong>{`${user.get('address').geo.lat} , ${user.get('address').geo.lng}`}</p>
 						</fieldset>
 					)}
 				</div>
@@ -65,8 +59,30 @@ class Profile extends Component {
 }
 
 Profile.propTypes = {
-	user: PropTypes.object,
-	match: PropTypes.object
+	user: PropTypes.shape({
+		id: PropTypes.number,
+		name: PropTypes.string,
+		size: PropTypes.number,
+		get: PropTypes.func
+	}),
+	match: PropTypes.shape({
+		params: PropTypes.shape({
+			id: PropTypes.string
+		})
+	}),
+	actions: PropTypes.objectOf(PropTypes.func)
 }
 
-export default Profile
+const mapStateToProps = (state, ownProps) => {
+	return {
+		user: state.get('users').get(Number(ownProps.match.params.id))
+	}
+}
+
+const mapDispatchToProps = (dispatch) => {
+	return {
+		actions: bindActionCreators(actions, dispatch)
+	}
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Profile)
